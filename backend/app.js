79e543c7e5c8e478e4705697c58d85667d19ca8d
Firsakov Dmitriy <firsakovds@mail.ru>
2023-07-16 00:58:22 +0300

    тест

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,7 @@ const cors = require("cors");
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 // Слушаем 3000 порт
 const { PORT = 3000,
-MONGO_URL = 'mongodb://127.0.0.1:27017'
+MONGO_URL = 'mongodb://localhost:27017'
 
 } = process.env;
 mongoose
